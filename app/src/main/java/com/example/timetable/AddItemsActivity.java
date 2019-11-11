2a1990dc0ba7689e96256ef6dbd5558316c36e6d
ShiryaevNikolay <deendeeps@list.ru>
2019-11-11 13:32:03 +0300

    Implemented adding items to the list in "add items"

--- app/src/main/java/com/example/timetable/AddItemsActivity.java
+++ app/src/main/java/com/example/timetable/AddItemsActivity.java
@@ -1,6 +1,8 @@
 package com.example.timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +12,9 @@ import android.widget.Toolbar;
 public class AddItemsActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+
+    private RecyclerView recyclerView;
+    private ItemsAdapter itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,19 @@ public class AddItemsActivity extends AppCompatActivity {
         toolbar.setNavigationIcon(R.drawable.toolbar_back_btn);
 
         goBackToMainActivity();
+
+        // Находим RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
+        // то, как будет выглядеть RecyclerView (то есть список)
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        // передаём layoutManager в RecyclerView
+        recyclerView.setLayoutManager(layoutManager);
+        // значит, что список фиксированный
+        recyclerView.setHasFixedSize(true);
+        // 5 - кол-во элементов в списке
+        itemsAdapter = new ItemsAdapter(5);
+        //назначаем RecyclerView созданный Adapter
+        recyclerView.setAdapter(itemsAdapter);
     }
 
     public void goBackToMainActivity(){
---
+++ app/src/main/java/com/example/timetable/dataModel.java
@@ -0,0 +1,13 @@
+package com.example.timetable;
+
+public class dataModel {
+    private String first;
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+}
