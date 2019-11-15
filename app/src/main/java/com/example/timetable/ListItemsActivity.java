23f0034376a38907f2cb4db0716021ed6f248e22
ShiryaevNikolay <deendeeps@list.ru>
2019-11-15 19:09:33 +0300

    Add Toolbar in AddItemsActivity

--- app/src/main/java/com/example/timetable/ListItemsActivity.java
+++ app/src/main/java/com/example/timetable/ListItemsActivity.java
@@ -1,5 +1,6 @@
 package com.example.timetable;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -30,7 +31,8 @@ public class ListItemsActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // тут событие при нажатии на кнопку
+                Intent intent = new Intent(ListItemsActivity.this, AddItemsActivity.class);
+                startActivity(intent);
             }
         });
 
