93dd60af10b43ae67bacef42bccdd4f11ca2197c
ShiryaevNikolay <deendeeps@list.ru>
2019-11-15 17:22:43 +0300

    Redesign the main buttons

--- app/src/main/java/com/example/timetable/ButtonToReturnToMainActivity.java
+++ app/src/main/java/com/example/timetable/ButtonToReturnToMainActivity.java
@@ -14,6 +14,7 @@ public class ButtonToReturnToMainActivity {
             @Override
             public void onClick(View v) {
                 launchMainActivity(activity);
+                activity.finish();
             }
         });
     }
