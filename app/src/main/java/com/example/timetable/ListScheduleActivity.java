113dc836a8ed99eeaec73faef9e3a7ce8dad1059
ShiryaevNikolay <deendeeps@list.ru>
2019-11-25 15:40:18 +0300

    Cleaned unnecessary code.

--- app/src/main/java/com/example/timetable/ListScheduleActivity.java
+++ app/src/main/java/com/example/timetable/ListScheduleActivity.java
@@ -19,16 +19,6 @@ import com.google.android.material.tabs.TabLayout;
 import java.util.List;
 
 public class ListScheduleActivity extends AppCompatActivity {
-//    public static final String ACCESS_MESSAGE_CLOCK="ACCESS_MESSAGE_CLOCK";
-//    public static final String ACCESS_MESSAGE_NAME="ACCESS_MESSAGE_NAME";
-//    public static final String ACCESS_MESSAGE_TEACHER="ACCESS_MESSAGE_TEACHER";
-//    public static final String ACCESS_MESSAGE_DAY="ACCESS_MESSAGE_DAY";
-
-    private static String daySchedule;
-    private List<RecyclerSchedule> listItems;
-    private SQLiteDatabase database;
-    private RecyclerView recyclerView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,43 +30,6 @@ public class ListScheduleActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.toolbar_back_btn);
         new ButtonToReturnToMainActivity(toolbar, this);
-
-//        //для базы данных
-//        ScheduleDBHelper scheduleDBHelper = new ScheduleDBHelper(this);
-//        database = scheduleDBHelper.getWritableDatabase();
-//        @SuppressLint("Recycle") Cursor cursor = database.query(ScheduleDBHelper.TABLE_SCHEDULE, null, null, null, null, null, null);
-//
-//        listItems = new ArrayList<>();
-//        // добавляем в список данные (названия предметов) из базы данных
-//        if (cursor.moveToFirst()){
-//            do {
-//                // проверяем, совпадает ли день недели в базе данных с днём, куда добавляем данные
-//                if (cursor.getString(cursor.getColumnIndex(ScheduleDBHelper.KEY_DAY)).equals(daySchedule)){
-//                    clockSchedule = cursor.getString(cursor.getColumnIndex(ScheduleDBHelper.KEY_CLOCK));
-//                    nameSchedule = cursor.getString(cursor.getColumnIndex(ScheduleDBHelper.KEY_NAME));
-//                    teacherSchedule = cursor.getString(cursor.getColumnIndex(ScheduleDBHelper.KEY_TEACHER));
-//                    listItems.add(new RecyclerSchedule(clockSchedule, nameSchedule, teacherSchedule));
-//                }
-//            }while (cursor.moveToNext());
-//        }
-//
-//        // Находим RecyclerView
-//        recyclerView = findViewById(R.id.recyclerView_schedule);
-//        // то, как будет выглядеть RecyclerView (то есть список)
-//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-//        // передаём layoutManager в RecyclerView
-//        recyclerView.setLayoutManager(layoutManager);
-//        // значит, что список фиксированный
-//        recyclerView.setHasFixedSize(true);
-//
-//        // numberItems - кол-во элементов в списке, nameItem - название предмета
-//        ScheduleAdapter itemsAdapter = new ScheduleAdapter(listItems, database);
-//        //назначаем RecyclerView созданный Adapter
-//        recyclerView.setAdapter(itemsAdapter);
-//
-//        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(itemsAdapter);
-//        ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
-//        touchHelper.attachToRecyclerView(recyclerView);
     }
 
     private void initTabs() {
@@ -91,31 +44,6 @@ public class ListScheduleActivity extends AppCompatActivity {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data){
-//        if(requestCode == RequestCode.REQUEST_CODE_SCHEDULE){
-//            //для добавление в список и базы данных
-//            String clockSchedule;
-//            String nameSchedule;
-//            String teacherSchedule;
-//            if(resultCode==RESULT_OK){
-//                if (daySchedule.equals(data.getStringExtra(ACCESS_MESSAGE_DAY))){
-//                    clockSchedule = data.getStringExtra(ACCESS_MESSAGE_CLOCK);
-//                    nameSchedule = data.getStringExtra(ACCESS_MESSAGE_NAME);
-//                    teacherSchedule = data.getStringExtra(ACCESS_MESSAGE_TEACHER);
-//                    listItems.add(new RecyclerSchedule(clockSchedule, nameSchedule, teacherSchedule));
-//                }
-//            }
-//            else{
-//                clockSchedule = "hh:mm";
-//                nameSchedule = "Ошибка доступа";
-//                teacherSchedule = "Ошибка доступа";
-//            }
-//        }
-//        else{
             super.onActivityResult(requestCode, resultCode, data);
-//        }
-//
-//        ScheduleAdapter itemsAdapter = new ScheduleAdapter(listItems, database);
-//        //назначаем RecyclerView созданный Adapter
-//        recyclerView.setAdapter(itemsAdapter);
     }
 }
