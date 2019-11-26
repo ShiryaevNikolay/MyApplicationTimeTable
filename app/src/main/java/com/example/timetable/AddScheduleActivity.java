32df3d7c2c4c3129a7dc74679f2909f04b991599
ShiryaevNikolay <deendeeps@list.ru>
2019-11-26 22:47:35 +0300

    Changed the timing in schedule creation.

--- app/src/main/java/com/example/timetable/AddScheduleActivity.java
+++ app/src/main/java/com/example/timetable/AddScheduleActivity.java
@@ -3,6 +3,8 @@ package com.example.timetable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
@@ -13,16 +15,23 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.example.timetable.database.ScheduleDBHelper;
 import com.example.timetable.fragments.ScheduleFragment;
 
-public class AddScheduleActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AddScheduleActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String day = "";
     private String clock = "";
     private String name = "";
     private String teacher = "";
+
+    private TextView tvClock;
+    private int hour, minute;
 
     //==============================================================================================
     ScheduleDBHelper scheduleDBHelper;
@@ -58,14 +67,18 @@ public class AddScheduleActivity extends AppCompatActivity {
         //__________________________________________________________________________________________
         //__________________________________________________________________________________________
         // находим поля с вводом текста "Фамилия, имя, отчество"
-        final EditText etClock = findViewById(R.id.editText_clock_schedule);
+//        final EditText etClock = findViewById(R.id.editText_clock_schedule);
         final EditText etName = findViewById(R.id.editText_name_schedule);
         final EditText etTeacher = findViewById(R.id.editText_teacher_schedule);
 
         // проверяем, пустое ли поле ввода
-        checkEmptyField(etClock, okBtn);
+//        checkEmptyField(etClock, okBtn);
         checkEmptyField(etName, okBtn);
         checkEmptyField(etTeacher, okBtn);
+
+        tvClock = findViewById(R.id.tv_clock_schedule);
+
+        tvClock.setOnClickListener(this);
         //__________________________________________________________________________________________
         //__________________________________________________________________________________________
         //__________________________________________________________________________________________
@@ -80,55 +93,81 @@ public class AddScheduleActivity extends AppCompatActivity {
         });
 
         // при нажатии на "Ок", отправляется текст в ListItemActivity и закрывается текущее окно activity
-        okBtn.setOnClickListener(new View.OnClickListener() {
+//        okBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (!etClock.getText().toString().equals("") || !etName.getText().toString().equals("") || !etTeacher.getText().toString().equals("")){
+//                    clock = etClock.getText().toString();
+//                    name = etName.getText().toString();
+//                    teacher = etTeacher.getText().toString();
+//
+//                    contentValues.put(ScheduleDBHelper.KEY_DAY, day);
+//                    contentValues.put(ScheduleDBHelper.KEY_CLOCK, clock);
+//                    contentValues.put(ScheduleDBHelper.KEY_NAME, name);
+//                    contentValues.put(ScheduleDBHelper.KEY_TEACHER, teacher);
+//
+//                    //вставляем данные в таблицу базы данных
+//                    database.insert(ScheduleDBHelper.TABLE_SCHEDULE, null, contentValues);
+//
+//                    // ВЫВОД ДАННЫХ ИЗ БАЗЫ ДАННЫХ В ТЕРМИНАЛ
+//                    //==============================================================================
+//                    Cursor cursor = database.query(ScheduleDBHelper.TABLE_SCHEDULE, null, null, null, null, null, null);
+//
+//                    if (cursor.moveToFirst()){
+//                        int dayIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_DAY);
+//                        int idIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_ID);
+//                        int clockIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_CLOCK);
+//                        int nameIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_NAME);
+//                        int teacherIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_TEACHER);
+//                        do {
+//                            System.out.println("ID = " + cursor.getInt(idIndex) +
+//                                    ", day = " + cursor.getString(dayIndex) +
+//                                    ", clock = " + cursor.getString(clockIndex) +
+//                                    ", name = " + cursor.getString(nameIndex) +
+//                                    ", teacher = " + cursor.getString(teacherIndex));
+//                            }while (cursor.moveToNext());
+//                    } else {
+//                        System.out.println("0 rows");
+//                    }
+//
+//                    cursor.close();
+//                    scheduleDBHelper.close();
+//                    //==============================================================================
+//
+//                    sendMessage(day, clock, name, teacher);
+//                } else {
+//                    clock = "";
+//                    name = "";
+//                    teacher = "";
+//                }
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.tv_clock_schedule:
+                callTimePicker();
+                break;
+        }
+    }
+
+    private void callTimePicker(){
+        // получаем текущее время
+        final Calendar calendar = Calendar.getInstance();
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
+
+        // инициализируем диалог выбора времени текущими значениями
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, AlertDialog.THEME_HOLO_LIGHT, new TimePickerDialog.OnTimeSetListener() {
             @Override
-            public void onClick(View v) {
-                if (!etClock.getText().toString().equals("") || !etName.getText().toString().equals("") || !etTeacher.getText().toString().equals("")){
-                    clock = etClock.getText().toString();
-                    name = etName.getText().toString();
-                    teacher = etTeacher.getText().toString();
-
-                    contentValues.put(ScheduleDBHelper.KEY_DAY, day);
-                    contentValues.put(ScheduleDBHelper.KEY_CLOCK, clock);
-                    contentValues.put(ScheduleDBHelper.KEY_NAME, name);
-                    contentValues.put(ScheduleDBHelper.KEY_TEACHER, teacher);
-
-                    //вставляем данные в таблицу базы данных
-                    database.insert(ScheduleDBHelper.TABLE_SCHEDULE, null, contentValues);
-
-                    // ВЫВОД ДАННЫХ ИЗ БАЗЫ ДАННЫХ В ТЕРМИНАЛ
-                    //==============================================================================
-                    Cursor cursor = database.query(ScheduleDBHelper.TABLE_SCHEDULE, null, null, null, null, null, null);
-
-                    if (cursor.moveToFirst()){
-                        int dayIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_DAY);
-                        int idIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_ID);
-                        int clockIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_CLOCK);
-                        int nameIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_NAME);
-                        int teacherIndex = cursor.getColumnIndex(ScheduleDBHelper.KEY_TEACHER);
-                        do {
-                            System.out.println("ID = " + cursor.getInt(idIndex) +
-                                    ", day = " + cursor.getString(dayIndex) +
-                                    ", clock = " + cursor.getString(clockIndex) +
-                                    ", name = " + cursor.getString(nameIndex) +
-                                    ", teacher = " + cursor.getString(teacherIndex));
-                            }while (cursor.moveToNext());
-                    } else {
-                        System.out.println("0 rows");
-                    }
-
-                    cursor.close();
-                    scheduleDBHelper.close();
-                    //==============================================================================
-
-                    sendMessage(day, clock, name, teacher);
-                } else {
-                    clock = "";
-                    name = "";
-                    teacher = "";
-                }
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                String editTextTimeParam = hourOfDay + " : " + minute;
+                tvClock.setText(editTextTimeParam);
             }
-        });
+        }, hour, minute, true);
+        timePickerDialog.show();
     }
 
     protected void checkEmptyField(final EditText editText, final Button btn){
